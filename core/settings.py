8d7c44d34df42a5b0fbc06a3e53d2d9323e47031
Vitória Melo <vitoriaolivermelo08@gmail.com>
2025-08-27 10:36:04 -0300

    feat: adicionar tailwind e flowbite

--- core/settings.py
+++ core/settings.py
@@ -32,6 +32,12 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+COMPRESS_ROOT = BASE_DIR / 'static'
+
+COMPRESS_ENABLED = True
+
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -41,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "compressor",
     "rodas",
 ]
 
